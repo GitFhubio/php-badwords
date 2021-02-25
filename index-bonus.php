5ed05de28e6f3c169cae09b2a304a43a208e0618
Fabio Petrone <fa610movic90@gmail.com>
2021-02-25 17:20:34 +0100

    tolto var dump versione base

--- index-bonus.php
+++ index-bonus.php
@@ -5,14 +5,20 @@
   <title>Php-badwords</title>
 </head>
 <body>
+
+  <form class="" action="index-bonus.php" method="post">
+   <input type="text" name="" value="">
+   <button type="submit" name="modulo"></button>
+  </form>
+
+
   <?php
 
-  // if ($_GET) {
-  //   echo 'Ci sono elementi in get';
-  // } else {
-  // echo 'Non ci sono elementi in get';
-  //
-  //   }
+  if (isset($_POST['modulo'])) {
+    echo "é stato effettuato il submit";
+    echo "Nome passato:{$_POST['nome']} "
+  }
+
   $text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   $newText = str_replace($_GET["badword"], "***", "$text");
