73da71a0696196e1118483213be4d3a9039b123d
Fabio Petrone <fa610movic90@gmail.com>
2021-02-25 17:39:47 +0100

    prova post

--- index-bonus.php
+++ index-bonus.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-  <form class="" action="index-bonus.php" method="post">
+  <form style="margin-bottom:10px;" class="" action="index-bonus.php" method="post">
    <input type="text" name="nome" placeholder="Inserisci la mala parola" value="">
    <button type="submit" name="modulo">clicca qui</button>
   </form>
@@ -14,17 +14,17 @@
 
   <?php
 
-  if ($_POST) {
-  echo 'Ci sono elementi in $_POST';
-  var_dump ($_POST);
-  } else {
-  echo 'Non ci sono elementi in $_POST';
-
-  }
-  if (isset($_POST['modulo'])) {
-    // echo "é stato effettuato il submit";
-    echo "Badword:{$_POST['nome']} ";
-  }
+  // if ($_POST) {
+  // echo 'Ci sono elementi in $_POST';
+  // var_dump ($_POST);
+  // } else {
+  // echo 'Non ci sono elementi in $_POST';
+  //
+  // }
+  // if (isset($_POST['modulo'])) {
+  //   // echo "é stato effettuato il submit";
+  //   echo "Badword:{$_POST['nome']} ";
+  // }
 
   $text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
@@ -32,7 +32,9 @@
   // var_dump(!$_GET);
   ?>
   <div>
-  <?php if (!$_POST || strpos($text, $_POST["nome"])==false) { ?>
+
+   <?php if (!$_POST || strpos($text, $_POST["nome"])==false) { echo "La mala parola ". $_POST['nome'] ." non è presente nel testo "; ?>
+
     <h1>Testo originale</h1>
     <p><?php echo $text; ?></p>
   <?php } else { ?>
