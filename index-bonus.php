2440cba40171ae4dba673e9e6af75c175fa3b3ec
Fabio Petrone <fa610movic90@gmail.com>
2021-02-25 17:22:57 +0100

    keep on

--- index-bonus.php
+++ index-bonus.php
@@ -1,3 +1,13 @@
+<?php
+
+if ($_POST) {
+echo 'Ci sono elementi in $_POST';
+} else {
+echo 'Non ci sono elementi in $_POST';
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
@@ -6,18 +16,18 @@
 </head>
 <body>
 
-  <form class="" action="index-bonus.php" method="post">
-   <input type="text" name="" value="">
+  <!-- <form class="" action="index-bonus.php" method="post">
+   <input type="text" name="nome" value="">
    <button type="submit" name="modulo"></button>
-  </form>
+  </form> -->
 
 
   <?php
 
-  if (isset($_POST['modulo'])) {
-    echo "é stato effettuato il submit";
-    echo "Nome passato:{$_POST['nome']} "
-  }
+  // if (isset($_POST['modulo'])) {
+  //   echo "é stato effettuato il submit";
+  //   echo "Nome passato:{$_POST['nome']} "
+  // }
 
   $text='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
